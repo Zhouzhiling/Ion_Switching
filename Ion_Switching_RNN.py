d30e58aa6a15dbc2fc9e50e19c4cc638d09144f7
柳洋 <yangliu2@caltech.edu>
2020-04-01 14:54:04 -0700

    add sketch code

---
+++ Ion_Switching_RNN.py
@@ -0,0 +1,116 @@
+from keras.layers import LSTM, Input, Dense, Lambda
+from keras.optimizers import RMSprop, Adam
+from keras import Model
+from keras.models import load_model
+from keras.callbacks import EarlyStopping, ReduceLROnPlateau, ModelCheckpoint
+import numpy as np
+
+
+class IonSwitchingLSTM(object):
+
+    def __init__(self):
+        self.window_size = 100
+        self.output_size = 1
+        pass
+
+    def load_data(self, path):
+        pass
+
+    def preprocess(self, path):
+        X, y = [], []
+        return X, y
+
+    def train(self, path):
+
+        X, y = self.preprocess(path)
+
+        input_data = Input(shape=(self.window_size, self.output_size,))
+
+        lstm = LSTM(
+            units=150,
+            activation='tanh',
+            recurrent_activation='sigmoid',
+            use_bias=True
+        )(input_data)
+
+        output = Dense(
+            units=self.output_size,
+            activation='softmax'
+        )(lstm)
+
+        model = Model(input_data, output)
+
+        optimizer = Adam(learning_rate=0.01)
+
+        model.compile(
+            loss='categorical_crossentropy',
+            optimizer=optimizer,
+            metrics=['accuracy']
+        )
+
+        model.summary()
+
+        callbacks = [
+            ModelCheckpoint(
+                'lstm_best.hdf5',
+                monitor='loss',
+                verbose=1,
+                save_best_only=True,
+                mode='auto',
+                period=1
+            ),
+            EarlyStopping(
+                monitor='loss',
+                patience=5,
+                mode='auto',
+                verbose=1
+            ),
+            ReduceLROnPlateau(
+                monitor='loss',
+                patience=3,
+                verbose=1,
+                mode='auto',
+                factor=0.1
+            )
+        ]
+
+        model.fit(
+            x=X,
+            y=y,
+            shuffle=True,
+            batch_size=32,
+            epochs=100,
+            callbacks=callbacks
+        )
+
+        model.save('lstm.hdf5')
+
+    def test(self, path):
+        X, y = self.preprocess(path)
+
+        input_data = Input(shape=(self.window_size, self.output_size,))
+
+        lstm = LSTM(
+            units=150,
+            activation='tanh',
+            recurrent_activation='sigmoid',
+            use_bias=True
+        )(input_data)
+
+        lstm = Lambda(
+            lambda x: x / 0.75
+        )(lstm)
+
+        output = Dense(
+            units=self.output_size,
+            activation='softmax'
+        )(lstm)
+
+        model = Model(input_data, output)
+        model.load_weights('lstm_best_model_weights.hdf5')
+
+
+if __name__ == '__main__':
+    ion_switching_lstm = IonSwitchingLSTM()
+    ion_switching_lstm.train('data/train.csv')
+    ion_switching_lstm.test('data/test.csv')
