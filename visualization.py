f389b1da015612da27699a9c89301a2a12094e6e
柳洋 <yangliu2@caltech.edu>
2020-04-01 14:13:24 -0700

    add visualization sketch

---
+++ visualization.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+
+class Visualization(object):
+
+    def __init__(self):
+        pass
+
+    def load_data(self):
+        pass
+
+    def print_statistics(self):
+        pass
+
+    def draw_statistics(self):
+        pass
