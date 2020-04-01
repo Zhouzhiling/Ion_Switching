e538781d8977fd3aebede3bf9bbe0a2da27b89a1
柳洋 <yangliu2@caltech.edu>
2020-04-01 14:45:18 -0700

    add basic visualization

--- visualization.py
+++ visualization.py
@@ -1,16 +1,22 @@
+import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 
 class Visualization(object):
 
-    def __init__(self):
-        pass
-
-    def load_data(self):
-        pass
+    def __init__(self, path):
+        self.data = pd.read_csv(path)
 
     def print_statistics(self):
         pass
 
     def draw_statistics(self):
-        pass
+        g = sns.relplot(x='time', y='signal', hue='open_channels', size='open_channels', data=self.data[:][:-1:10000])
+        plt.show()
+
+
+if __name__ == '__main__':
+    vi = Visualization('data/train.csv')
+    vi.draw_statistics()
